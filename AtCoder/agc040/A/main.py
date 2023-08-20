7d6e6dad92fd52dba2a82b7d1e864df8a174edcd
Yasuharu Sawada <yasuharu519@gmail.com>
2023-08-20 17:20:34 +0900

    Update

--- AtCoder/agc040/A/main.py
+++ AtCoder/agc040/A/main.py
@@ -3,33 +3,31 @@ import sys
 
 
 def solve(S: str):
-    current = 0
-    result = 0
-    smallest = 0
+    n = len(S)
+    a = [0] * (n + 1)
 
-    for i in S:
-        if i == "<":
-            current += 1
-        else:
-            current -= 1
-        result += current
-        smallest = min(smallest, current)
-    result += current
-    
-    if smallest < 0:
-        diff = 0 - smallest
-        result += diff * (len(S) + 1)
-    print(result)
+    for i in range(n):
+        if S[i] == "<":
+            a[i + 1] = max(a[i + 1], a[i] + 1)
+    for i in range(n - 1, -1, -1):
+        if S[i] == ">":
+            a[i] = max(a[i], a[i + 1] + 1)
+
+    print(sum(a))
+    return
 
 
 def main():
+
     def iterate_tokens():
         for line in sys.stdin:
             for word in line.split():
                 yield word
+
     tokens = iterate_tokens()
     S = next(tokens)  # type: str
     solve(S)
+
 
 if __name__ == '__main__':
     main()
