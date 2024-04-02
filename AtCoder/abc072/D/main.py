a2bf7b43f1af6ca0df0d3e9c003b1998cc44fedc
Yasuharu Sawada <yasuharu519@gmail.com>
2024-04-02 10:12:33 +0900

    Update

--- AtCoder/abc072/D/main.py
+++ AtCoder/abc072/D/main.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-import sys
+from typing import List
 
-
-def solve(N: int, p: "List[int]"):
+def solve(N: int, p: List[int]):
     count = 0
-
     for i in range(N-1):
         if p[i] == i+1:
             p[i], p[i+1] = p[i+1], p[i]
             count += 1
-
+    if p[N-1] == N:
+        count += 1
     print(count)
     return
+
 
 def main():
     N = int(input().strip())
